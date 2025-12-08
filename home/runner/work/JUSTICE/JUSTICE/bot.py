7921d25ef820d58b0a1713853dacb637e900f2df
ivanm696 <59553806+ivanm696@users.noreply.github.com>
2025-12-08 12:55:47 +0200

    Update bot.py
    [![Bot CI/CD Workflow](https://github.com/ivanm696/JUSTICE/actions/workflows/deploy.yml/badge.svg)](https://github.com/ivanm696/JUSTICE/actions/workflows/deploy.yml)
    
    Signed-off-by: ivanm696 <59553806+ivanm696@users.noreply.github.com>

--- home/runner/work/JUSTICE/JUSTICE/bot.py
+++ home/runner/work/JUSTICE/JUSTICE/bot.py
@@ -1,4 +1,4 @@
-[Bot CI/CD Workflow](https://github.com/ivanm696/JUSTICE/actions/workflows/deploy.yml/badge.svg)](https://github.com/ivanm696/JUSTICE/actions/workflows/deploy.yml)
+[![Bot CI/CD Workflow](https://github.com/ivanm696/JUSTICE/actions/workflows/deploy.yml/badge.svg)](https://github.com/ivanm696/JUSTICE/actions/workflows/deploy.yml)
 import asyncio
 import json
 import math
